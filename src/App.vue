0de1c99a049e9ded0e2415ec655d02c54e5195c1
Lightdev2 <stas12102001@gmail.com>
2021-09-14 17:42:48 +0900

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="app-wrapper">
+    <div class="header">
+      <div class="header__name">Имя</div>
+      <div class="header__lenght">Размер</div>
+      <div class="header__type">Тип</div>
+    </div>
+    <FolderView name="root" :folders="folders" :files="files" :isOpen="true" />
+  </div>
+</template>
+
+<script>
+import FolderView from "./components/FolderView.vue";
+import data from "./data/list";
+
+export default {
+  name: "App",
+  components: {
+    FolderView,
+  },
+  data: () => data,
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+html {
+  background-color: rgb(13, 17, 23);
+}
+.app-wrapper {
+  width: 60%;
+  margin: 0 auto;
+  border-radius: 5px;
+  border: 1px solid;
+  border-color: grey;
+  margin-top: 50px;
+  @media screen and (max-width: 1024px) {
+    width: 80%;
+  }
+}
+.header {
+  padding-left: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  display: flex;
+  border-bottom: 2px solid grey;
+  margin-bottom: 5px;
+  font-size: 18px;
+  color: rgb(186, 193, 194);
+  &__name {
+    width: 50%;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+  &__lenght {
+    width: 15%;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+  &__type {
+    width: 35%;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+}
+</style>
